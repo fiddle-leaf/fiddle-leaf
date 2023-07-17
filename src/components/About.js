623b9a68b11286c4f1734264b01c2083772bec12
fiddle.leaf <shanonps@icloud.com>
2023-07-17 01:14:41 -0400

    Minor changes to UI

--- src/components/About.js
+++ src/components/About.js
@@ -1,13 +1,11 @@
 export default function About () {
     return ( 
+        <>
+        <h4>About Me</h4>
         <aside className="about">
             <div className="icon">
                 <img src="https://i.imgur.com/IGc0i1o.gif" alt=""></img>
             </div>
-            <div className="about-text">
-            <h4>
-                About Me
-            </h4>
             <ul className="about-list">
                 <li>
                     <mark id="dev-name">Shanon</mark>, 26 year old Plant Mom.
@@ -17,7 +15,7 @@ export default function About () {
                 <li>Missing my hometown Lima every single day.</li>
                 <li>Follow me on my journey with UI Design.</li>
             </ul>
-            </div>
         </aside>
+        </>
      );
 }
